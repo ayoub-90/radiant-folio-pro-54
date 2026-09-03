afbc3cad9948c11b615e739857664446682c8844
Ayoub Elharem <84919952+ayoub-90@users.noreply.github.com>
2026-09-03 17:32:15 +0000

    commit for vercel.json and vite.config.ts changes

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
-    server: { entry: "server" },
+    //server: { entry: "server" },
     base: '/radiant-folio-pro-54/',
     plugins: [react()],
   },
