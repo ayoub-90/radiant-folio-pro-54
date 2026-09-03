7f14d797f8944855ca7c8711654e0a2aa7c017ca
Ayoub Elharem <84919952+ayoub-90@users.noreply.github.com>
2026-09-03 16:49:27 +0000

    Updates

--- server/assets/_tanstack-start-manifest_v-Di_oJn5A.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/workspaces/radiant-folio-pro-54/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-CZtj7_0N.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-CZtj7_0N.js" } }] }, "/": { filePath: "/workspaces/radiant-folio-pro-54/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CLypwRc0.js"] } } });
-export {
-  tsrStartManifest
-};
